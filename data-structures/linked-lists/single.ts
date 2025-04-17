92a04d10f9bf2b70f28e4d89b41526d2e4d47a4d
marcell ciszek <ciszekmarcell@gmail.com>
2025-04-17 11:37:46 +0200

    Refactor List class to use private fields and improve method implementations

--- data-structures/linked-lists/single.ts
+++ data-structures/linked-lists/single.ts
@@ -19,35 +19,39 @@ interface ListInterface {
 	toString(): string;
 	reverse(): void;
 	sort(): void;
-	print: () => void;
 }
 
 export class List implements ListInterface {
-	private head: Node | null;
-	private length: number;
+	#head: Node | null;
+	#length: number;
 	constructor() {
-		this.head = null;
-
-		this.length = 0;
+		this.#head = null;
+		this.#length = 0;
 	}
+
 	add(value: number): void {
 		let newNode = new Node(value);
-		if (this.head === null) {
-			// list is empty
-			this.head = newNode;
+		if (this.#head === null) {
+			this.#head = newNode;
 		} else {
-			let current = this.head;
-			// let last = null;
+			let current = this.#head;
 			while (current.next !== null) {
 				current = current.next;
 			}
-			console.log("current", current, newNode);
 			current.next = newNode;
 		}
-		this.length += 1;
+		this.#length += 1;
 	}
 	prepend(value: number): void {
-		throw new Error("Method not implemented.");
+		let newNode = new Node(value);
+		if (this.#head === null) {
+			this.#head = newNode;
+		} else {
+			let current = this.#head;
+			newNode.next = current;
+			this.#head = newNode;
+		}
+		this.#length += 1;
 	}
 	remove(value: number): void {
 		throw new Error("Method not implemented.");
@@ -56,10 +60,10 @@ export class List implements ListInterface {
 		throw new Error("Method not implemented.");
 	}
 	isEmpty(): boolean {
-		throw new Error("Method not implemented.");
+		return this.#head === null;
 	}
 	size(): number {
-		throw new Error("Method not implemented.");
+		return this.#length;
 	}
 	clear(): void {
 		throw new Error("Method not implemented.");
@@ -68,7 +72,7 @@ export class List implements ListInterface {
 		throw new Error("Method not implemented.");
 	}
 	toString(): string {
-		let current = this.head;
+		let current = this.#head;
 		if (current === null) return "List is empty";
 		let res = [];
 		while (current) {
@@ -83,7 +87,6 @@ export class List implements ListInterface {
 	sort(): void {
 		throw new Error("Method not implemented.");
 	}
-	print() {
-		//
-	}
+
+	// private methods!
 }
